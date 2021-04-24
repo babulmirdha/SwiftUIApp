6fbbc1dd5f4f7c436632ae996e3adcb90994708b
Babul Mirdha <babumirdha@gmail.com>
2021-04-24 22:33:24 +0600

    Initial Commit

---
+++ AlorFeriIosApp/AlorFeriIosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlorFeriIosAppApp.swift
+//  AlorFeriIosApp
+//
+//  Created by Babul Mirdha on 24/4/21.
+//
+
+import SwiftUI
+
+@main
+struct AlorFeriIosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlorFeriIosApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlorFeriIosApp
+//
+//  Created by Babul Mirdha on 24/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
